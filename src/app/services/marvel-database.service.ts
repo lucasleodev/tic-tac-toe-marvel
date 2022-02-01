5d94947fa22303103df5b62e87bada5faf4ae143
Lucas Xavier <lucasxavier92@gmail.com>
2022-02-01 16:53:18 -0300

    dados voltando da API, renomeados valores em environment

--- src/app/services/marvel-database.service.ts
+++ src/app/services/marvel-database.service.ts
@@ -2,15 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
+import { Md5 } from 'ts-md5/dist/md5';
+
 @Injectable({
   providedIn: 'root',
 })
 export class MarvelDatabaseService {
   constructor(private http: HttpClient) {}
 
-  searchMarvelCharacter(name: string = '') {
-    this.http.get(environment.URL_MARVEL).subscribe((res) => {
-      console.log(res);
-    });
+  searchMarvelCharacter(hero?: string) {
+    let ts = new Date().getTime();
+    let tempHash = ts + environment.PRIVATE_KEY + environment.PUBLIC_KEY;
+    let hash = Md5.hashStr(tempHash);
+    let params = {
+      ts: ts,
+      apikey: environment.PUBLIC_KEY,
+      hash: hash,
+    };
+    params = hero
+      ? Object.assign({}, params, { name: hero })
+      : Object.assign({}, params);
+    this.http
+      .get(`${environment.API_MARVEL}/v1/public/characters`, { params })
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  URL_MARVEL: 'https://gateway.marvel.com',
-  API_MARVEL: 'ea5da9083cd02368ea2b57c078ae66f9',
+  API_MARVEL: 'https://gateway.marvel.com',
+  PUBLIC_KEY: 'ea5da9083cd02368ea2b57c078ae66f9',
+  PRIVATE_KEY: '3d4a64bad36e86dad1dd10c3407dd79363fd53b7',
 };
